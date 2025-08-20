9fb4e8db6ace8a27b402825c59dd4442637287f0
Nantha001 <pulsernantha123@gmail.com>
2025-08-20 15:58:30 +0530

    v1

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const sqlite3 = require("sqlite3").verbose();
+
+app.use(express.json());
+
+// Database initialization
+const db = new sqlite3.Database("./mydb.sqlite", (err) => {
+    if (err) {
+        console.log(err.message);
+    }
+    console.log("Connected to sqlite database");
+});
+
+db.run(`DROP TABLE IF EXISTS users`);
+db.run(`CREATE TABLE users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    content TEXT
+)`);
+
+
+// POST method (Insert user)
+app.post("/users", (req, res) => {
+    const { name, content } = req.body;
+    db.run(`INSERT INTO users(name, content) VALUES(?, ?)`,
+        [name, content],
+        function (err) {
+            if (err) {
+                return res.status(400).json({ error: err.message });
+            }
+            res.send(this.lastID);
+        }
+    );
+});
+
+// GET method (Fetch all users)
+app.get("/", (req, res) => {
+    db.all(`SELECT * FROM users`, [], (err, rows) => {
+        if (err) {
+            return res.status(500).json({ error: "Error fetching data" });
+        }
+        res.json(rows);
+    });
+});
+
+// Start server
+app.listen(3000, () => {
+    console.log("Server is running at port 3000");
+});
